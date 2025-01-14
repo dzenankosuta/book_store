0aecae7482f676ec72e018d7c268f6e4ab55be65
dzenankosuta <dzenan_kosuta@hotmail.com>
2025-01-14 08:50:54 +0100

    Rendering the Details Page

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
+from django.http import Http404
 
 # Create your views here.
 
@@ -9,3 +10,15 @@ def index(request):
     "books": books
   })
 
+def book_detail(request, id):
+  # try: 
+  #   book = Book.objects.get(pk=id)
+  # except:
+  #   raise Http404()
+  book = get_object_or_404(Book, pk=id)
+  return render(request, "book_detail.html", {
+    "title": book.title,
+    "author": book.author,
+    "rating": book.rating,
+    "is_bestseller": book.is_bestselling
+  })
