ddba17a5fb4f8d8763658825b94e1e2ff9f05d8b
dzenankosuta <dzenan_kosuta@hotmail.com>
2025-01-17 11:36:14 +0100

    Configuring Model Fields

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -10,7 +10,7 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)
+    slug = models.SlugField(default="", null=False, db_index=True, blank=True, editable=False)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
